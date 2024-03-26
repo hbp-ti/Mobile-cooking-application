9bece0f0acd2dfb887fbe8a2cad35b6469ca71a3
Hugo Pereira <hbp@ua.pt>
2024-03-26 17:48:23 +0000

    added scrollview  and added the rest of the activities

---
+++ SideChefSource/SideChef/app/src/main/java/dadm/cooking/sidechef/click_recipe_details.kt
@@ -0,0 +1,20 @@
+package dadm.cooking.sidechef
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class click_recipe_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_click_recipe_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
