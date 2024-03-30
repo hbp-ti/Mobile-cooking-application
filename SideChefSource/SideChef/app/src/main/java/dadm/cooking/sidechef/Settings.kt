1b41b28018bb342378759cf436338c96163410ea
Hugo Pereira <hbp@ua.pt>
2024-03-30 17:53:39 +0000

    fix button settings

--- SideChefSource/SideChef/app/src/main/java/dadm/cooking/sidechef/Settings.kt
+++ SideChefSource/SideChef/app/src/main/java/dadm/cooking/sidechef/Settings.kt
@@ -1,8 +1,10 @@
 package dadm.cooking.sidechef
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat
 class Settings : AppCompatActivity() {
 
     private lateinit var backButton: ImageButton
+    private lateinit var editTextCurrentName: EditText
+    private lateinit var imageViewEditName: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +30,22 @@ class Settings : AppCompatActivity() {
         backButton.setOnClickListener {
             changeToProfile()
         }
+
+        editTextCurrentName = findViewById(R.id.textViewCurrentName)
+        imageViewEditName = findViewById(R.id.imageViewEditName)
+
+        imageViewEditName.setOnClickListener {
+            editTextCurrentName.isEnabled = true
+            editTextCurrentName.requestFocus()
+            editTextCurrentName.setSelection(editTextCurrentName.text.length)
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(editTextCurrentName, InputMethodManager.SHOW_IMPLICIT)
+        }
+
     }
 
     private fun changeToProfile() {
         finish()
     }
 }
+
